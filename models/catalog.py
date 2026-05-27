af6533762c95d1eed80dc9d742f81eb36854afe3
Nqobile-Nda <ndabezithanqobile27@gmail.com>
2026-05-27 23:31:49 +0200

    Remove redundant database connection function from catalog.py

--- models/catalog.py
+++ models/catalog.py
@@ -1,12 +1,4 @@
-import sqlite3
-
-
-def database_connection():
-    conn = sqlite3.connect("CBL.db")
-    conn.row_factory = sqlite3.Row
-    conn.execute("PRAGMA foreign_keys = ON")
-    cur = conn.cursor()
-    return conn, cur
+from models.db import database_connection
 
 
 def catalog_table():
