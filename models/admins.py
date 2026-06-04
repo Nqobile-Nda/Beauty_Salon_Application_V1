84cf89bd0b7fcc73802e55d29fb773d6691c917d
Nqobile-Nda <ndabezithanqobile27@gmail.com>
2026-06-04 23:29:41 +0200

    An admin.py model has been created, this will create the admin's table.

---
+++ models/admins.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+def database_connection():
+    conn = sqlite3.connect("CBL.db")
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor
+    return conn, cur
+
+
+def admins_table():
+    conn, cur = database_connection()
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS admins_table (
+    admin_id INT PRIMARY KEY AUTOINCREMENT,
+    full name TEXT NOT NULL,
+    role TEXT NOT NULL,
+    password TEXT NOT NULL,
+    login_history TEXT NOT NULL
+    )
+    """)
+    conn.close()
+    return
