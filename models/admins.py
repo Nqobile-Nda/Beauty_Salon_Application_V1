1a77cf3d20ca08413586718de52bdfa2caca1d57
Nqobile-Nda <ndabezithanqobile27@gmail.com>
2026-06-05 20:05:46 +0200

    Fix cursor initialization and correct column name in admins_table creation

--- models/admins.py
+++ models/admins.py
@@ -3,7 +3,7 @@ import sqlite3
 def database_connection():
     conn = sqlite3.connect("CBL.db")
     conn.row_factory = sqlite3.Row
-    cur = conn.cursor
+    cur = conn.cursor()
     return conn, cur
 
 
@@ -11,12 +11,12 @@ def admins_table():
     conn, cur = database_connection()
     cur.execute("""
     CREATE TABLE IF NOT EXISTS admins_table (
-    admin_id INT PRIMARY KEY AUTOINCREMENT,
-    full name TEXT NOT NULL,
+    admin_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    full_name TEXT NOT NULL,
     role TEXT NOT NULL,
     password TEXT NOT NULL,
     login_history TEXT NOT NULL
     )
     """)
+    conn.commit()
     conn.close()
-    return
